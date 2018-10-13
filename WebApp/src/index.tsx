cc5177eb5eb0c8ce7afba26219842aaeab0611ad
Alexander Kehr <alex@alexanderkehr.de>
2018-10-13 14:04:05 +0200

    Setting up npm, react, redux typescript, webpack for web app :rocket:

---
+++ WebApp/src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+// import { Provider } from 'react-redux';
+
+// import App from './components/app';
+// import configureStore from './state/configureStore';
+
+render(
+  // <Provider store={configureStore()}>
+  //   <App />
+  // </Provider>,
+  <div>Fix Brain Fix Brain Fix</div>,
+  document.getElementById('root'),
+);
---
+++ WebApp/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'templates/index.html'
+    })
+  ]
+};
